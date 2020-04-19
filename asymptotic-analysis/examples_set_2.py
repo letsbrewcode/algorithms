0083e87311b8d78d43b8f7ed53ab525d68515d69
letsbrewcode <50429097+letsbrewcode@users.noreply.github.com>
2020-04-19 14:40:00 -0700

    Create examples_set_2.py

---
+++ asymptotic-analysis/examples_set_2.py
@@ -0,0 +1,50 @@
+# Determine the time complexity of following algorithms
+# Purpose of this file is to practice calculating
+# time and space complexity of basic algorithms.
+# NOTE - This code may not be syntactically correct
+# . Its only for mathematical calculations and is
+# not expected to be run.
+
+# Example 1
+def func(n):
+    for i in range(n):
+        for j in range(n):
+            z = 2 * 2
+
+# Example 2
+def func(n):
+    for i in range(n):
+        z = 2 * 2
+
+# Example 3
+def func(n):
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                z = 2 * 2
+
+# Example 4
+def func(n):
+    i = n
+    while i > 0:
+        z = 2 * 2
+        i = i // 2
+
+# Example 5
+def func(n):
+    for i in range(n):
+        z = 2 * 2
+    for j in range(n):
+        z = 2 * 2
+    for k in range(n):
+        z = 2 * 2
+
+# Example 6
+def func(n):
+    for i in range(n):
+        for j in range(n):
+            z = i * i
+            y = j * j
+
+    for k in range(n):
+        x = k * k + 45
